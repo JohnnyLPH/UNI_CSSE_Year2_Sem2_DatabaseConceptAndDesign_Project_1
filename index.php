8154c1ef5719a38b6922d3ba62a5481c7c27accf
JRDrazypoffs <dragon.jp49@gmail.com>
2022-06-12 18:15:11 +0800

    some testing (login interface)

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
-<?php include "dbConnection.php"; ?>
+<?php include "dbConnection.php";?>
+
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <title>Main Page</title>
+        <title>Index</title>
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta charset="utf-8">
         
         <!-- <link rel="stylesheet" href="/css/main.css"> -->
-        <!-- <link rel="shortcut icon" href="/favicon.ico"> -->
+        <link rel="shortcut icon" href="https://icon-library.com/images/tree-icon/tree-icon-23.jpg">
     </head>
 
     <body>
@@ -16,6 +18,8 @@
         </header>
 
         <main>
+        <button><a href="login.php">LOGIN</a></button>
+        <button><a href="adminDashboard.php">ADMIN DASHBOARD</a></button>
             
         </main>
 
