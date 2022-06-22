6f747e2e88c07ee3f8e632a69dec9a722b4fe928
Lau, P. H <70358914+JohnnyLPH@users.noreply.github.com>
2022-06-22 20:51:53 +0800

    again no change

---
+++ Company/manageTree.php
@@ -0,0 +1,44 @@
+<?php
+    // Company Manage Tree Page.
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/dbConnection.php");
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/loginAuthenticate.php");
+    require_once($_SERVER['DOCUMENT_ROOT'] . "/dataRetrieval.php");
+
+    $tempLoginCheck = checkLogin($conn);
+    // Not logged in as Company.
+    if ($tempLoginCheck != 1) {
+        header("Location: /index.php");
+        exit;
+    }
+
+    $conn->close();
+?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Company: Manage Tree Page</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="utf-8">
+        
+        <link rel="stylesheet" href="/css/main.css">
+        <!--<link rel="shortcut icon" href="/favicon.ico">-->
+        <link rel="shortcut icon" href="https://icon-library.com/images/tree-icon/tree-icon-23.jpg">
+    </head>
+
+    <body>
+        <header>
+            <h1>Company: Manage Tree Page</h1>
+        </header>
+
+        <?php include($_SERVER['DOCUMENT_ROOT'] . "/Company/navigationBar.php"); ?>
+
+        <main>
+            <h2>Tree:</h2>
+
+        </main>
+
+        <footer>
+            
+        </footer>
+    </body>
+</html>
