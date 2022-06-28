6690c1cf5e1411dac3c9c3002ba9eec8730641b3
JRDrazypoffs <dragon.jp49@gmail.com>
2022-06-28 23:30:27 +0800

    testing sales report (WIP) + ihave no idea how to make it dynamic :'(

--- Admin/reportSales.php
+++ Admin/reportSales.php
@@ -20,6 +20,12 @@
     $totalTree = getTreeCount($conn);
     $totalPurchase = getPurchaseRequestCount($conn, 1);
 
+    $allCompany = getAllCompany($conn);
+
+    foreach ($allCompany as $eachCompany){
+        $listCompanyID[] = $eachCompany["UserID"];
+    }
+
     $conn->close();
 ?>
 <!DOCTYPE html>
@@ -71,17 +77,17 @@
                     <h2>Summary of Related Sales</h2>
                 </div>
 
-                <div class="w3-container w3-threequarter wrapper bgImgTree w3-animate-left" style="margin-left:25%;">
+                <!--<div class="w3-container w3-threequarter wrapper bgImgTree w3-animate-left" style="margin-left:25%;">
                     <div class='data-value card fadeIn'>
                         <div class='data-group'>
                             <label>This is a card tag</label>
                         </div>
                     </div>
-                </div>
+                </div>-->
 
                 <div class="w3-container w3-threequarter wrapper w3-animate-left w3-theme-l5" style="margin-left:25%;">
-                    <h2>Related Sales Report</h2>
-                    <canvas id="chart1" style="width:100%;"></canvas>
+                    <h3><b>Monthly Average Sales </b></h3>
+                    <canvas id="chart" style="height:450px;width:100%;"></canvas>
                 </div>
             </div>
         </main>
@@ -91,36 +97,135 @@
         </footer>
 
         <script>
-            const labels = [
-                'A',
-                'B',
-                'C',
-                'D',
-                'E',
-                'F',
-            ];
+            companyID = <?php echo json_encode($listCompanyID); ?>;            
 
-            const data = {
-                labels: labels,
+            //graph for company sales
+
+            /*const data0 = {
+                labels: companyID,
                 datasets: [{
-                label: 'Trees',
-                backgroundColor: 'rgb(0, 131, 115, 0.7)',
-                borderColor: 'rgb(0, 82, 72)',
-                data: [0, 10, 5, 2, 20, 30, 45],
+                    label: '1',
+                    backgroundColor: 'rgb(0, 131, 115, 0.7)',
+                    borderColor: 'rgb(0, 82, 72)',
+                    data: trees,
+                },{
+                    label: '2',
+                    backgroundColor: 'rgb(0, 131, 115, 0.7)',
+                    borderColor: 'rgb(0, 82, 72)',
+                    data: trees,
                 }]
             };
 
-            const config = {
-                type: 'bar',
-                data: data,
+            const config0 = {
+                type: 'line',
+                data: data0,
                 options: {
                     indexAxis: 'y',
+                    scales: {
+                        x: {
+                            title: {
+                                display: true,
+                                text: 'Average Monthly Block Price (RM)'
+                            }
+                        },
+                        y: {
+                            title: {
+                                display: true,
+                                text: 'Company ID'
+                            },
+                        }
+                    }
                 }
             };
-            const chart1 = new Chart(
-                document.getElementById('chart1'),
+
+            const chart0 = new Chart(
+                document.getElementById('chart0'),
+                config0
+            );*/
+
+            //config
+            const config = {
+                type: 'line',
+                data: data,
+                options: {
+                    responsive: true,
+                    interaction: {
+                    mode: 'index',
+                    intersect: false,
+                    },
+                    stacked: false,
+                    plugins: {
+                    title: {
+                        display: true,
+                        text: 'Chart.js Line Chart - Multi Axis'
+                    }
+                    },
+                    scales: {
+                    y: {
+                        type: 'linear',
+                        display: true,
+                        position: 'left',
+                    },
+                    y1: {
+                        type: 'linear',
+                        display: true,
+                        position: 'right',
+
+                        // grid line settings
+                        grid: {
+                        drawOnChartArea: false, // only want the grid lines for one axis to show up
+                        },
+                    },
+                    }
+                },
+            };
+            
+            //setup
+            const DATA_COUNT = 7;
+            const NUMBER_CFG = {count: DATA_COUNT, min: -100, max: 100};
+
+            const labels = Utils.months({count: 7});
+            const data = {
+                labels: labels,
+                datasets: [
+                    {
+                    label: 'Dataset 1',
+                    data: Utils.numbers(NUMBER_CFG),
+                    borderColor: Utils.CHART_COLORS.red,
+                    backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
+                    yAxisID: 'y',
+                    },
+                    {
+                    label: 'Dataset 2',
+                    data: Utils.numbers(NUMBER_CFG),
+                    borderColor: Utils.CHART_COLORS.blue,
+                    backgroundColor: Utils.transparentize(Utils.CHART_COLORS.blue, 0.5),
+                    yAxisID: 'y1',
+                    }
+                ]
+            };
+
+            const actions = [
+                {
+                    name: 'Randomize',
+                    handler(chart) {
+                    chart.data.datasets.forEach(dataset => {
+                        dataset.data = Utils.numbers({count: chart.data.labels.length, min: -100, max: 100});
+                    });
+                    chart.update();
+                    }
+                },
+            ];
+
+            const chart = new Chart(
+                document.getElementById('chart0'),
                 config
             );
+
+
+
+
+
         </script>
     </body>
 </html>
